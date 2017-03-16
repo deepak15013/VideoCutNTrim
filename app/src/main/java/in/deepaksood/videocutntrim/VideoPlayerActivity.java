d98ff43e2e3154f1ab045583dd88eceb329d1d0b
Deepak Kumar Sood <deepak15013@iiitd.ac.in>
2017-03-16 08:31:21 +0530

    Added Browse file functionality

---
+++ app/src/main/java/in/deepaksood/videocutntrim/VideoPlayerActivity.java
@@ -0,0 +1,13 @@
+package in.deepaksood.videocutntrim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
