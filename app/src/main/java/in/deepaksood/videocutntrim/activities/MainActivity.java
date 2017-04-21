70f4ab9311740fb31d6ae26b0e0a96809f7ad608
Deepak <deepak15013@iiitd.ac.in>
2017-04-21 08:50:59 +0530

    Added intents to other activity

--- app/src/main/java/in/deepaksood/videocutntrim/activities/MainActivity.java
+++ app/src/main/java/in/deepaksood/videocutntrim/activities/MainActivity.java
@@ -1,6 +1,7 @@
 package in.deepaksood.videocutntrim.activities;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -8,17 +9,24 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import in.deepaksood.videocutntrim.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     // TAG for logging
     private static final String TAG = MainActivity.class.getSimpleName();
 
     // constant for storing the runtime permission access for external storage media
     private static final int MY_PERMISSION_WRITE_EXTERNAL_STORAGE = 44;
+
+    // initializing the buttons
+    private Button btnCreateStory;
+    private Button btnEditStory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +36,11 @@ public class MainActivity extends AppCompatActivity {
         /* First check for permission for external storage, Uses runtime permission */
         checkAndGetRuntimePermissions();
 
+        btnCreateStory = (Button) findViewById(R.id.btn_create_story);
+        btnEditStory = (Button) findViewById(R.id.btn_edit_story);
 
+        btnCreateStory.setOnClickListener(this);
+        btnEditStory.setOnClickListener(this);
     }
 
     /**
@@ -68,6 +80,27 @@ public class MainActivity extends AppCompatActivity {
                     finish();
                 }
             }
+        }
+    }
+
+    /**
+     * onClick Listener for buttons
+     * @param view
+     */
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_create_story:
+                Toast.makeText(this, "Create Story", Toast.LENGTH_SHORT).show();
+                Intent intentCreateStory = new Intent(this, CreateStory.class);
+                startActivity(intentCreateStory);
+                break;
+
+            case R.id.btn_edit_story:
+                Toast.makeText(this, "Edit Story", Toast.LENGTH_SHORT).show();
+                Intent intentEditStory = new Intent(this, EditStory.class);
+                startActivity(intentEditStory);
+                break;
         }
     }
 }
