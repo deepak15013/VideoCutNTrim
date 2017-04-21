ecf981d4714d51e33bd85887ae4de3971bd4cdb6
Deepak <deepak15013@iiitd.ac.in>
2017-04-21 08:51:56 +0530

    CreateStory activity added

---
+++ app/src/main/java/in/deepaksood/videocutntrim/activities/CreateStory.java
@@ -0,0 +1,15 @@
+package in.deepaksood.videocutntrim.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import in.deepaksood.videocutntrim.R;
+
+public class CreateStory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_story);
+    }
+}
